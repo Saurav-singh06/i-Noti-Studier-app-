20b4504a956542c7b6790635672069c843de7fd6
Saurav singh <sauravsingh85079@gmail.com>
2021-12-02 01:09:32 +0530

    Add Auth.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+app.use(express.json())
 //Available Routes
 app.use('/api/auth',require ('./routes/auth'))
 app.use('/api/notes',require ('./routes/notes'))
@@ -12,7 +13,6 @@ app.use('/api/notes',require ('./routes/notes'))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const User = require('../Models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
-    obj ={
-        a:'saurav',
-        number:43
+
+
+// Create a User using Post "/api/auth". Doesnt require Auth
+router.post('/',[
+    body('email','Enter a valid Email').isEmail(),
+    body('name','Enter a valid name').isLength({ min: 3 }),
+    body('password','Password must be atleast 5 characters').isLength({ min: 5 })
+
+],(req,res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(obj)
-    
+    User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email
+    }).then(user => res.json(user));
+    res.send(req.body)
 })
 
 module.exports=router
