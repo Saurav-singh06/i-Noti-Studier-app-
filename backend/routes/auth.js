3230f8b062fb40bd126b6c1bb54d46d016a6a914
Saurav singh <sauravsingh85079@gmail.com>
2021-12-31 17:22:12 +0530

    Add user Validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,24 +5,31 @@ const { body, validationResult } = require('express-validator');
 
 
 
-// Create a User using Post "/api/auth". Doesnt require Auth
-router.post('/',[
+// Create a User using Post "/api/auth/createUser". No Login required
+router.post('/createUser',[
     body('email','Enter a valid Email').isEmail(),
     body('name','Enter a valid name').isLength({ min: 3 }),
     body('password','Password must be atleast 5 characters').isLength({ min: 5 })
 
-],(req,res)=>{
-
+], async (req,res)=>{ 
+// if there are error,return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    // Check whether the email exist already 
+    let user = await user.findOne({email : req.body.email}) 
+    if (user){
+      return res.status(400).json({error : "Sorry a user with this email already exists"})
+    }
+    user = await User.create({
       name: req.body.name,
       password: req.body.password,
       email: req.body.email
-    }).then(user => res.json(user));
-    res.send(req.body)
+    })
+    // .then(user => res.json(user))
+    // .catch(err => {console.log(err)
+    // res.json({error: 'Plese enter a unique value',message: err.message})})
 })
 
 module.exports=router
